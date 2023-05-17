f6afbf8da0f622f5eb486bc4e90ebbb1d7ee69e1
qubit26 <58095357+qubit26@users.noreply.github.com>
2023-05-17 17:53:00 -0400

    cambiando db

--- api_agenda/db.py
+++ api_agenda/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # DATABASE_URI = 'postgresql://postgres:admin@localhost:5432/agenda'
-DATABASE_URI = 'postgres://angel:NOH6KYGp8sqb4s6bLRLY4QlD9xS49b7q@dpg-chhujn2k728p863o4ukg-a/curso_db_udemy'
+DATABASE_URI = 'postgres://angel:NOH6KYGp8sqb4s6bLRLY4QlD9xS49b7q@dpg-chhujn2k728p863o4ukg-a.oregon-postgres.render.com/curso_db_udemy'
 
 def conectar():
     engine = create_engine(DATABASE_URI)
